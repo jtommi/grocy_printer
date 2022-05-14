3940941e5edf13d091ec4d570573be5754c41134
Thomas Johanns <johanns.thomas@gmail.com>
2022-05-14 11:41:37 +0200

    Move printer initialisation

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ template = load_template("template.zpl")
 name_split = int(os.getenv("LINELENGTH", default=15))
 printer_name = os.getenv("PRINTER", default="zebra")
 
+printer = Zebra(printer_name)
 
 @app.post("/printer/product", status_code=201)
 async def print_product(product: Product):
     label = generate_label(template, product, name_split)
 
-    printer = Zebra("zebra")
     printer.output(label)
 
     return {"zpl": label}
