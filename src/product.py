8fb348c1f9e563253732aa2adf6dc5cba3157fab
Thomas Johanns <johanns.thomas@gmail.com>
2025-08-05 21:48:35 +0200

    Remove font family

--- src/product.py
+++ src/product.py
@@ -1,10 +1,7 @@
-from typing import Optional
-
 from pydantic import BaseModel, Field
 
 
 class Product(BaseModel):
     name: str = Field(alias="product")
     grocycode: str
-    font_family: Optional[str] = None
     due_date: str
